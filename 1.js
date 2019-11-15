b0277d03fcce803b875331f1ee01aa3ba6e9c007
Nguyen Long Dang <you@example.com>
2019-11-15 20:54:52 +0700

    thay cv

--- 1.js
+++ 1.js
@@ -1,1 +1,15 @@
-﻿
+﻿$(function() {
+	$('.navbar-toggler.hidden-sm-up').click(function(event) {
+		/* Act on the event */
+		$('#header .collapse.navbar-toggleable-xs').addClass('hienmenu');
+		$('#header .collapse.navbar-toggleable-xs').removeClass('anmenu');
+
+	});
+
+	$('.btn.btn-outline-danger.btn-block').click(function(event) {
+		/* Act on the event */
+		$('#header .collapse.navbar-toggleable-xs').addClass('anmenu');
+		$('#header .collapse.navbar-toggleable-xs').removeClass('hienmenu');
+		return false;
+	});
+});
